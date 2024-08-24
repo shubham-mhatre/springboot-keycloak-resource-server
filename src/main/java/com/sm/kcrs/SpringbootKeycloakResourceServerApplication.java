50cee3992e8b51c569494ab0f9df83a89248766b
shubham mhatre <mhatreshubham03@gmail.com>
2024-08-24 13:06:07 +0530

    spring boot keycloak resource server initial setup

---
+++ src/main/java/com/sm/kcrs/SpringbootKeycloakResourceServerApplication.java
@@ -0,0 +1,13 @@
+package com.sm.kcrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootKeycloakResourceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootKeycloakResourceServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sm/kcrs/SpringbootKeycloakResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sm.kcrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootKeycloakResourceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
